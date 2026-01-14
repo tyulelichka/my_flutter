1af54844082c1fa9d2286b623c4dc783b1bd5ab9
Viktoriia Kozlova <viktoriia.kozlova5@gmail.com>
2026-01-14 11:54:17 +0100

    removed the commented code

--- lib/data/app_constants.dart
+++ lib/data/app_constants.dart
@@ -1,13 +1,5 @@
-// import 'package:hive/hive.dart';
-// import 'package:todolist/data/to_do_list.dart';
-
 abstract class AppConstantsString {
   static const toDoTaskBoxName = 'to_do_task';
   static const toDoCategoryBoxName = 'to_do_category';
   static const initialIcon = 'all';
 }
-// abstract class AppConstantsBox {
-//   static Box<ToDoTask> listTasksBox = Hive.box<ToDoTask>(
-//     AppConstantsString.toDoTaskBoxName,
-//   );
-// }
